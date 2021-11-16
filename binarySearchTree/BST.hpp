6c230d2b45376b62a79e46ea28113e0e42e16920
A01751676 <A01751676@itesm.mx>
2021-11-16 13:31:09 -0600

    programa

---
+++ binarySearchTree/BST.hpp
@@ -0,0 +1,227 @@
+#include "NodoBST.hpp"
+#include "fila.hpp"
+
+template <typename T>
+class BST{
+    NodoBST<T> * raiz;
+    int numNodos;
+    public:
+        //getters y setters
+        NodoBST<T> * getRaiz(){return this->raiz;}
+        void setRaiz(NodoBST<T> * raiz){this->raiz=raiz;}
+        int getNumNodos(){return this->numNodos;}
+        void setNumNodos(int numNodos){this->numNodos=numNodos;} 
+        //constructor
+        BST(){
+            this->raiz=nullptr;
+            this->numNodos=0;
+        }
+        ~BST(){
+
+        }
+
+        void agregarNodo(T dato){
+            NodoBST<T> * actual= this->raiz;
+            NodoBST<T> * padre;
+            int nivel = 0;
+            if(this->raiz){//Arbol no vacio
+                if(this->buscarNodo(dato)) //Nodo encontrado
+                    cout<<"Nodo existente"<<endl;                
+                else{ //Nodo inexistente, 
+                    //Encontrar al nodo padre
+                    while(actual){
+                        padre=actual;
+                        //Relacion de orden de los nodos
+                        if(dato<actual->getDato())
+                            actual=actual->getIzq();
+                        else
+                            actual=actual->getDer();
+                        nivel++;
+                    }
+                    //Colocar el nuevo nodo
+                    padre->getDato()>dato?padre->setIzq(new NodoBST<T>(padre,dato,nivel)):padre->setDer(new NodoBST<T>(padre,dato,nivel));
+                    this->numNodos++;
+                }
+            }else{//Arbol vacio
+                this->raiz = new NodoBST<T>(nullptr,dato, nivel);
+                this->numNodos++;
+                }
+            
+            return; //opcional
+        }
+        NodoBST<T> * buscarNodo(T dato){
+            //Primer paso crear el apuntador a nodo raiz
+            NodoBST<T> * actual = this->raiz;
+            while(actual){
+                //Si el nodo actual es el valor que busco
+                if(actual->getDato()==dato)
+                    return actual;
+                else//cond?opc_verdadera:opc_falsa
+                    actual=actual->getDato()>dato?actual->getIzq():actual->getDer();
+            }
+            return nullptr; //return actual;
+        }
+
+        void imprimirInOrder(){
+            NodoBST<T>::recorridoInOrder(this->raiz);
+            cout << endl;
+        }
+
+        void imprimirPreOrden(){
+            NodoBST<T>::recorridoPreOrden(this->raiz);
+            cout << endl;
+        }
+
+        void imprimirPostOrden(){
+            NodoBST<T>::recorridoPostOrden(this->raiz);
+            cout << endl;
+        }
+
+        void eliminarNodo(T dato){
+            NodoBST<T> * eliminar = this->buscarNodo(dato);
+            NodoBST<T> * padre = nullptr;
+
+            if(eliminar){
+                padre = eliminar->getPadre();
+
+                // caso 1: eliminar raiz
+                if(!eliminar->getIzq() && !eliminar->getDer()){
+                    if(!padre){ // si es nodo raiz
+                        this->raiz = nullptr;
+                    }
+
+                    else if (padre->getDato() > eliminar->getDato()){ // nodo hoja es hijo izq
+                        padre->setIzq(nullptr);
+                    }
+
+                    else{ // nodo hoja es hijo derecho
+                        padre->setDer(nullptr); 
+                    }
+
+                    this->numNodos--;
+                    delete(eliminar);
+                }
+
+                // Caso 2: Eliminar nodo con 1 hijo
+                
+                else if(eliminar->getIzq() && !eliminar->getDer()){ // hijo izquierdo
+                    if (!padre){
+                        this->raiz = eliminar->getIzq();
+                    }
+                    else if (padre->getDato() > eliminar->getDato()){ // eliminar es izquierdo
+                        padre->setIzq(eliminar->getIzq()); // izq izq
+                    }
+                    else{
+                        padre->setDer(eliminar->getIzq());
+                    }
+
+                    eliminar->getIzq()->setPadre(padre);
+                    eliminar->getIzq()->setNivel(padre->getNivel()+1);
+                    delete(eliminar);
+                    numNodos --;
+                }
+
+                else if (!eliminar->getIzq() && eliminar->getDer()){ // hijo derecho
+                    
+                    if (!padre){
+                        this->raiz = eliminar->getDer();
+                    }
+                    else if (padre->getDato() > eliminar->getDato()){ // eliminar es izquierdo
+                        padre->setDer(eliminar->getDer()); 
+                    }
+                    else{
+                        padre->setIzq(eliminar->getDer());
+                    }
+
+                    eliminar->getDer()->setPadre(padre);
+                    eliminar->getDer()->setNivel(padre->getNivel()+1);
+                    delete(eliminar);
+                    numNodos --;
+
+                }
+
+                else{ //  Caso 3: tiene dos hijos
+
+                    // encontrar el sucesor
+                    NodoBST<T> * sucesor = NodoBST<T>::valormenor(eliminar->getDer());
+                    eliminar->setValor(sucesor->getDato());
+
+                    if(sucesor->getPadre()->getIzq() == sucesor){
+                        if (sucesor->getDer()){
+                            sucesor->getPadre()->setIzq(sucesor->getDer());
+                            sucesor->getDer()->setPadre(sucesor->getPadre());
+                        }
+                        else{
+                            sucesor->getPadre()->setIzq(nullptr);
+                        }
+                    }
+
+                    else if (sucesor->getDer()){
+                            sucesor->getPadre()->setDer(sucesor->getDer());
+                            sucesor->getDer()->setPadre(sucesor->getPadre());
+                        }
+                    else{
+                        sucesor->getPadre()->setDer(nullptr);
+                    }
+                    delete(sucesor);
+                }
+                numNodos--;
+            }
+            else{
+                cout << "Valor inexistente" << endl;
+            }
+        }
+
+        int whatlevelamI(T dato){
+            if (this->buscarNodo(dato)){
+                return this->buscarNodo(dato)->getNivel();
+            }
+            else{
+                return -1;
+            }
+        }
+
+        NodoBST<T>* antecesor(NodoBST<T>* dato){
+            if(this->buscarNodo(dato)){
+                return NodoBST<T>::valormayor(dato->getIzq());
+            }
+            else{return nullptr;}
+        }
+
+        NodoBST<T>* sucesor(NodoBST<T>* dato){
+            if(this->buscarNodo(dato)){
+                return NodoBST<T>::valormenor(dato->getDer());
+            }
+            else{return nullptr;}
+        }
+
+        void byLevel(){
+            Fila<NodoBST<T>*> fila;
+            NodoBST<T>* dato = this->raiz;
+            
+            if(dato){
+                fila.push(dato);
+                cout << fila.front()->getDato()->getDato();
+                
+                while(fila.front()->getDato()){
+                    cout << fila.front()->getDato()->getDato() << " ";
+
+                    /*
+                    if (dato->getIzq()){
+                        fila->push(dato->getIzq());
+                    }
+
+                    if (dato->getDer()){
+                        fila->push(dato->getDer());
+                    }
+
+                    */
+                    dato = fila.pop()->getDato();
+                }
+                //dato = fila.pop()->getDato();
+                cout << endl;
+            }
+            
+            
+        }
+};
---
+++ binarySearchTree/main.cpp
@@ -0,0 +1,29 @@
+#include "BST.hpp"
+
+int main(){
+    BST<int> * arbol= new BST<int>();
+    arbol->agregarNodo(21);
+    arbol->agregarNodo(13);
+    arbol->agregarNodo(10);
+    arbol->agregarNodo(18);
+    arbol->agregarNodo(33);
+    arbol->agregarNodo(25);
+    arbol->agregarNodo(40);
+
+    arbol->imprimirInOrder();
+    arbol->imprimirPreOrden();
+    arbol->imprimirPostOrden();
+
+    arbol->eliminarNodo(18);
+    arbol->imprimirInOrder();
+
+    arbol->eliminarNodo(13);
+    arbol->imprimirInOrder();
+
+    arbol->eliminarNodo(33);
+    arbol->imprimirInOrder();
+
+    cout << arbol->whatlevelamI(25) << endl;
+    arbol->byLevel();
+    return 0;
+}
